8564cab67dc21982368ed77ba0022d5dcdc72dd9
SlavaLatokhin <st091713@student.spbu.ru>
2020-10-02 17:17:11 +0600

    Latokhin Svyatoslav Homework

--- src/main.c
+++ src/main.c
@@ -10,37 +10,37 @@
 
 double methode_rectangle(int n, double l_b, double r_b) {
 	double integral = 0;
-	double h = (r_b - l_b) / n;
-	double x = l_b;
+	double height = (r_b - l_b) / n;
+	double length_of_part = l_b;
 	for (int i = 0; i < n; i++) {
-		integral += sin(x + h / 2) * h;
-		x += h;
+		integral += sin(length_of_part + height / 2) * height;
+		length_of_part += height;
 	}
 	return integral;
 }
 
 double methode_simps(int n, double l_b, double r_b) {
-	double h = (r_b - l_b) / 2 / n;
+	double height = (r_b - l_b) / 2 / n;
 	double integral = sin(l_b) + sin(r_b);
-	double x = l_b + h;
+	double length_of_part = l_b + height;
 	for (int i = 1; i < 2 * n; i++) {
-		if (i % 2 == 0) integral += 2 * sin(x);
-		else integral += 4 * sin(x);
-		x += h;
+		if (i % 2 == 0) integral += 2 * sin(length_of_part);
+		else integral += 4 * sin(length_of_part);
+		length_of_part += height;
 	}
-	return (integral *= h / 3);
+	return (integral *= height / 3);
 }
 
-int vizov(int a[], double l_b, double r_b, char answer_vizov[][25]) {
+int vizov(int ans[], double l_b, double r_b, char answer_vizov[][25]) {
 	for (int i = 0; i < 6; i++) {
 		int j = 0;
-		double integral_rectangle = methode_rectangle(a[i], l_b, r_b);
-		double integral_simpson = methode_simps(a[i], l_b, r_b);
-		if (!sprintf(answer_vizov[i], "%d %2.5f %2.5f", a[i], integral_rectangle, integral_simpson)) {
+		double integral_rectangle = methode_rectangle(ans[i], l_b, r_b);
+		double integral_simpson = methode_simps(ans[i], l_b, r_b);
+		if (!sprintf(answer_vizov[i], "%d %2.5f %2.5f", ans[i], integral_rectangle, integral_simpson)) {
 			printf("Cannot write results to string in %d experiment\n", i);
 			return -1;
 		}
-		a[i] = 0;
+		ans[i] = 0;
 	}
 	return 0;
 }
@@ -81,7 +81,7 @@ int entering_numbers(double* l_b, double* r_b) {
 
 int main()
 {
-	int number_of_segments[] = {5, 10, 20, 100, 500, 1000};
+	int number_of_segments[] = { 6, 10, 20, 100, 500, 1000 };
 	double left_border, right_border;
 	char answer[6][25];
 	if (entering_numbers(&left_border, &right_border)) {
