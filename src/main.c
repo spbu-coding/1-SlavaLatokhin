1275c7eff195c489f84662fdff53bcc5c7932058
SlavaLatokhin <st091713@student.spbu.ru>
2020-10-05 20:25:57 +0600

    My corrections

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 ﻿#include <stdio.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
+#include <stdio.h>
+#include <malloc.h>
 
-#define PI  3.1415926
+#define printf(...) (fprintf(stderr, __VA_ARGS__))
 
-#define error(...) (fprintf(stderr, __VA_ARGS__))
+#define MAX_LENGTH_OF_STRING 25
 
-#pragma warning(disable : 4996)
-#pragma warning(disable : 6031)
+#pragma warning(disable : 4996) /*special for visual studio*/
+#pragma warning(disable : 6031) /*special for visual studio*/
 
-double methode_rectangle(int n, double l_b, double r_b) {
+double integrate_with_methode_rectangle(int n, double l_b, double r_b) {
 	double integral = 0;
 	double height = (r_b - l_b) / n;
 	double length_of_part = l_b;
@@ -19,7 +22,7 @@ double methode_rectangle(int n, double l_b, double r_b) {
 	return integral;
 }
 
-double methode_simps(int n, double l_b, double r_b) {
+double integrate_with_methode_simps(int n, double l_b, double r_b) {
 	double height = (r_b - l_b) / 2 / n;
 	double integral = sin(l_b) + sin(r_b);
 	double length_of_part = l_b + height;
@@ -31,66 +34,83 @@ double methode_simps(int n, double l_b, double r_b) {
 	return (integral *= height / 3);
 }
 
-int vizov(int ans[], double l_b, double r_b, char answer_vizov[][25]) {
-	for (int i = 0; i < 6; i++) {
-		int j = 0;
-		double integral_rectangle = methode_rectangle(ans[i], l_b, r_b);
-		double integral_simpson = methode_simps(ans[i], l_b, r_b);
-		if (!sprintf(answer_vizov[i], "%d %2.5f %2.5f", ans[i], integral_rectangle, integral_simpson)) {
+void free_answers(char** answers, unsigned int count) {
+	for (unsigned int i = 0; i < count; ++i) {
+		free(answers[i]);
+	}
+	free(answers);
+}
+
+char** calculate(int ans[], double l_b, double r_b, int size_of_array_answer) {
+	char** answers = (char**)malloc(sizeof(char*) * size_of_array_answer);
+	if (!answers) {
+		return NULL;
+	}
+	for (int i = 0; i < size_of_array_answer; i++) {
+		double integral_rectangle = integrate_with_methode_rectangle(ans[i], l_b, r_b);
+		double integral_simpson = integrate_with_methode_simps(ans[i], l_b, r_b);
+		answers[i] = (char*)malloc(sizeof(char) * MAX_LENGTH_OF_STRING);
+		if (!answers[i]) {
+			free_answers(answers, i);
+			printf("Cannot allocate memory for result string in %d experiment\n", i);
+			return NULL;
+		}
+		if (!sprintf(answers[i], "%-4d %.5f %.5f", ans[i], integral_rectangle, integral_simpson)) {
+			free_answers(answers, i + 1);
 			printf("Cannot write results to string in %d experiment\n", i);
-			return -1;
+			return NULL;
 		}
 		ans[i] = 0;
 	}
-	return 0;
+	return answers;
 }
 
 int entering_numbers(double* l_b, double* r_b) {
 	if (printf("Enter interval's left border: ") < 0) {
-		error("Cannot write to stdout\n");
+		printf("Cannot write to stdout\n");
 		return -1;
 	}
 	if (scanf("%lf", l_b) != 1) {
-		error("Cannot read interval's left border\n");
+		printf("Cannot read interval's left border\n");
 	}
 	if (*l_b < 0) {
-		error("Left border of the interval must be greater or equal than 0\n");
+		printf("Left border of the interval must be greater or equal than 0\n");
 		return -1;
 	}
-	if (*l_b > PI) {
-		error("Left border of the interval must be lower or equal than PI\n");
+	if (*l_b > M_PI) {
+		printf("Left border of the interval must be lower or equal than PI\n");
 		return -1;
 	}
 	if (printf("Enter interval's right border: ") < 0) {
-		error("Cannot write to stdout\n");
+		printf("Cannot write to stdout\n");
 		return -1;
 	}
 	if (scanf("%lf", r_b) != 1) {
-		error("Cannot read interval's right border\n");
+		printf("Cannot read interval's right border\n");
 	}
-	if (*r_b > PI) {
-		error("Right border of the interval must be less or equal than pi\n");
+	if (*r_b > M_PI) {
+		printf("Right border of the interval must be less or equal than pi\n");
 		return -1;
 	}
 	if (*r_b < *l_b) {
-		error("Right border of the interval must be greater than left\n");
+		printf("Right border of the interval must be greater than left\n");
 		return -1;
 	}
 	return 0;
 }
 
-int main()
-{
+int main() {
 	int number_of_segments[] = { 6, 10, 20, 100, 500, 1000 };
+	int size_of_array_answer = (sizeof(number_of_segments) / sizeof(number_of_segments[0]));
 	double left_border, right_border;
-	char answer[6][25];
 	if (entering_numbers(&left_border, &right_border)) {
 		return 1;
 	}
-	if (vizov(number_of_segments, left_border, right_border, answer)) {
+	char** answer = calculate(number_of_segments, left_border, right_border, size_of_array_answer);
+	if (!answer) {
 		return 1;
 	}
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < size_of_array_answer; i++) {
 		if (printf("%s\n", answer[i]) < 0) {
 			printf("Cannot write %d answer to stdout\n", i);
 			break;
